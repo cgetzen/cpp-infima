12b96b3c5b341da5229a13699890f7613036b430
Charlie Getzen <charliegetzenlc@gmail.com>
2021-03-09 23:51:29 -0600

    add correct fitness
    Signed-off-by: Charlie Getzen <charliegetzenlc@gmail.com>

--- performance/main.cc
+++ performance/main.cc
@@ -1,5 +1,7 @@
 #include "infima/infima.h"
 
+#include <algorithm>
+
 int main() {
   std::vector<std::string> dimensions {"AZ"};
   Lattice<int> lattice = Lattice<int>(dimensions);
@@ -7,4 +9,9 @@ int main() {
   lattice.add_endpoints_for_sector(Lattice<int>::Coordinate{"us-east-1a"}, std::vector<int>{2,3,4,5});
   ShuffleSharder<int> sharder = ShuffleSharder<int>(5353);
   Lattice<int> * shard = sharder.shuffleShard(lattice, 1, 2);
+  std::vector<int> endpoints = shard->get_endpoints();
+  std::vector<int> selection;
+  std::mt19937 g(5353);
+  std::sample(endpoints.begin(), endpoints.end(), std::back_inserter(selection), 2, g);
+  auto chosen = std::min_element(selection.begin(), selection.end());
 }
